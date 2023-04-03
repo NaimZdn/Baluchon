67f470ba171c1c3216f01299876266f47baeb0f1
Namsco <naim.zidouni@outlook.fr>
2023-04-03 17:05:13 +0200

    Initial Commit

---
+++ Baluchon/BaluchonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaluchonApp.swift
+//  Baluchon
+//
+//  Created by Zidouni on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaluchonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
