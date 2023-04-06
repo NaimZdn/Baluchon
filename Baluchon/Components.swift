26b8fe9fcd9bbde3ad7c7f5f4c0d2962984cacdb
Namsco <naim.zidouni@outlook.fr>
2023-04-06 22:15:38 +0200

    feat (translation) : add language choice button component

---
+++ Baluchon/Components.swift
@@ -0,0 +1,44 @@
+//
+//  Components.swift
+//  Baluchon
+//
+//  Created by Zidouni on 06/04/2023.
+//
+
+import SwiftUI
+
+struct TranslationButton: View {
+    var text = "French"
+    var image = "french"
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 10) {
+            Image(image)
+                .resizable()
+                .frame(width: 24, height:24)
+            Text(text)
+                .font(.defaultSecondaryText)
+                .foregroundColor(.black)
+                .baselineOffset(-7)
+            
+            
+        }
+        
+        .frame(minWidth: 120, maxHeight: 24, alignment: .leading)
+        .padding(.horizontal, 20)
+        .padding(.vertical, 15)
+        .background(
+            Color.secondaryLight,
+            in: RoundedRectangle(
+                cornerRadius: 15,
+                style: .continuous))
+        
+        
+    }
+}
+
+struct Components_Previews: PreviewProvider {
+    static var previews: some View {
+        TranslationButton()
+    }
+}
