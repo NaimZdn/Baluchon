23dd39f42b32317b29ecfddf0ee9f80610420371
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-17 08:17:16 +0200

    feat(converter) : add a button to change the translation order of the languages

--- Baluchon/Presentation/Translation/View/TranslationView.swift
+++ Baluchon/Presentation/Translation/View/TranslationView.swift
@@ -9,22 +9,31 @@ import SwiftUI
 
 struct TranslationView: View {
     @State private var textInput = ""
+    @State private var isReversed = false
     
     var body: some View {
         ScrollView {
             VStack {
                 HStack {
-                    TranslationButton(text: "French", image: "french")
-                    Image(systemName: "arrow.left.arrow.right")
-                        .foregroundColor(Color.iconLight)
-                        .font(.system(size: 15))
-                    
-                    TranslationButton(text: "English (US)", image: "usa")
+                    TranslationButton(text: "\(isReversed ? "English (US)" : "French")",
+                                      image: "\(isReversed ? "usa" : "french")")
+                    Button {
+                        withAnimation(.easeInOut(duration: 0.1)) {
+                            isReversed.toggle()
+                        }
+                    } label: {
+                        Image(systemName: "arrow.left.arrow.right")
+                            .foregroundColor(Color.iconLight)
+                            .font(.system(size: 15))
+                    }
+                    TranslationButton(text: "\(isReversed ? "French" : "English (US)")",
+                                      image: "\(isReversed ? "french" : "usa")")
+ 
                 }
                 .padding(.bottom, 20)
                 
-                TranslationTextField(lang: "French")
-                TranslationTextField(lang: "English")
+                TranslationTextField(lang: "\(isReversed ? "French" : "English")")
+                TranslationTextField(lang: "\(isReversed ? "English" : "French")")
             }
         }
         .padding(20)
