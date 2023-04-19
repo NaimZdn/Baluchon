0858711cb71f9cda9ffb9c1773813c4d78df03e7
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-19 22:59:26 +0200

    feat(traduction) : add traduction model

---
+++ Baluchon/Domain/Entities/Traduction.swift
@@ -0,0 +1,15 @@
+//
+//  Traduction.swift
+//  Baluchon
+//
+//  Created by Zidouni on 19/04/2023.
+//
+
+import Foundation
+
+struct Traduction {
+    let q: String
+    let source: String
+    let target: String
+    let format: String
+}
