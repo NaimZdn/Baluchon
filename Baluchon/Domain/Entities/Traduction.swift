c7c3e3cbe2590e28fa835e7c2c83e791a7ff3f2f
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-21 22:31:36 +0200

    feat(translation) : add 3 translation struct to retrieve and display data

--- Baluchon/Domain/Entities/Traduction.swift
+++ Baluchon/Domain/Entities/Traduction.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
-struct Traduction {
-    let q: String
-    let source: String
-    let target: String
-    let format: String
+struct TranslationResponse: Codable {
+    let data: TranslationData
+}
+
+struct TranslationData: Codable {
+    let translations: [TranslatedText]
+}
+
+struct TranslatedText: Codable {
+    let translatedText: String
 }
