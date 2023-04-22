9dda12624a0805a76b3c490362cb509efff08e60
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-22 21:10:17 +0200

    feat(view) : add a view extension to make the keyboard disappear when typing outside

---
+++ Baluchon/Extensions/View.swift
@@ -0,0 +1,15 @@
+//
+//  View.swift
+//  Baluchon
+//
+//  Created by Zidouni on 22/04/2023.
+//
+
+import Foundation
+
+extension View {
+  func endTextEditing() {
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                    to: nil, from: nil, for: nil)
+  }
+}
