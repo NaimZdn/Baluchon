d7f3060852a8160feb074f6aa6d9e6fc4997de81
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-24 00:55:32 +0200

    feat(translation) : add the API call for translation

--- Baluchon/Presentation/Translation/View/TranslationView.swift
+++ Baluchon/Presentation/Translation/View/TranslationView.swift
@@ -6,40 +6,77 @@
 //
 
 import SwiftUI
+import Combine
 
 struct TranslationView: View {
-    @State private var textInput = ""
+    @ObservedObject var viewModel = TranslationViewModel()
     @State private var isReversed = false
+    @State private var textToTranslate = ""
+    @State private var translatedString = ""
+    @State private var isDisabled = true
+    
+    private var englishId: String {
+        isReversed ? "en" : "fr"
+    }
+    
+    private var frenchId : String {
+        isReversed ? "fr" : "en"
+    }
     
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack {
-                    TranslationButton(text: "\(isReversed ? "English (US)" : "French")",
-                                      image: "\(isReversed ? "usa" : "french")")
-                    Button {
-                        withAnimation(.easeInOut(duration: 0.1)) {
-                            isReversed.toggle()
-                        }
-                    } label: {
-                        Image(systemName: "arrow.left.arrow.right")
-                            .foregroundColor(Color.iconColor)
-                            .font(.system(size: 15))
-                    }
-                    TranslationButton(text: "\(isReversed ? "French" : "English (US)")",
-                                      image: "\(isReversed ? "french" : "usa")")
- 
-                }
-                .padding(.bottom, 20)
+        VStack  {
+            HStack {
+                TranslationButton(text: "\(isReversed ? "English (US)" : "French")",
+                                  image: "\(isReversed ? "usa" : "french")",
+                                  id: englishId)
                 
-                TranslationTextField(lang: "\(isReversed ? "French" : "English")")
-                TranslationTextField(lang: "\(isReversed ? "English" : "French")")
+                Button {
+                    withAnimation(.easeInOut(duration: 0.1)) {
+                        isReversed.toggle()
+                        translatedString = viewModel.translatedText
+                        textToTranslate = translatedString
+                        
+                    }
+                    
+                } label: {
+                    Image(systemName: "arrow.left.arrow.right")
+                        .foregroundColor(Color.iconColor)
+                        .font(.system(size: 15))
+                }
+                
+                TranslationButton(text: "\(isReversed ? "French" : "English (US)")",
+                                  image: "\(isReversed ? "french" : "usa")",
+                                  id: frenchId)
+                
+            }
+            .padding(.top, 20)
+            .padding(.bottom, 10)
+            
+            VStack {
+                TranslationTextField(textInput: $textToTranslate, isDisabled: false)
+                
+                if !isDisabled {
+                    TranslationTextField(textInput: $viewModel.translatedText, isDisabled: true)
+                }
+            }
+            Spacer()
+        }
+        .onTapGesture {
+            self.endTextEditing()
+        }
+        .onReceive(Just(textToTranslate)) { text in
+            viewModel.translateText(text, source: isReversed ? "en" : "fr", target: isReversed ? "fr" : "en")
+            
+            if viewModel.translatedText.isEmpty {
+                isDisabled = true
+            } else {
+                isDisabled = false
             }
         }
-        .padding(20)
         .background(Color.backgroundColor)
     }
 }
+
 
 struct TranslationView_Previews: PreviewProvider {
     static var previews: some View {
