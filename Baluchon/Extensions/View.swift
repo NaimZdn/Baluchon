312acaf4732de6b80b97a2cd63572abc695f69a3
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-29 10:43:02 +0200

    feat(extension) : add a View extension to remove the keyboard when the user tap outside the textField

--- Baluchon/Extensions/View.swift
+++ Baluchon/Extensions/View.swift
@@ -9,8 +9,8 @@ import Foundation
 import SwiftUI
 
 extension View {
-  func endTextEditing() {
-    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
-                                    to: nil, from: nil, for: nil)
-  }
+    func endTextEditing() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
+                                        to: nil, from: nil, for: nil)
+    }
 }
