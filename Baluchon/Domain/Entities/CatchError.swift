141d16e19cd586fe34c5e3fa7385341f3949d96f
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-05-04 23:37:57 +0200

    feat(error) : add CatchError model to handle errors

---
+++ Baluchon/Domain/Entities/CatchError.swift
@@ -0,0 +1,31 @@
+//
+//  CatchError.swift
+//  Baluchon
+//
+//  Created by Zidouni on 04/05/2023.
+//
+
+import Foundation
+
+enum CatchError : Error {
+    case apiKeyNotFound
+    case exchangeRateAlreadyAvailable
+    case invalidCurrencyParameters
+    case unauthorizedAccess
+    case networkError
+    
+    var errorDescription: String {
+        switch self {
+        case .apiKeyNotFound:
+            return "Your APIKey was not found"
+        case .exchangeRateAlreadyAvailable:
+            return "You already have the exchange rate for this currency"
+        case .invalidCurrencyParameters:
+            return "Your base currency or convert currency is invalid "
+        case .unauthorizedAccess:
+            return "Access denied, please check your API Key"
+        case .networkError:
+            return ""
+        }
+    }
+}
