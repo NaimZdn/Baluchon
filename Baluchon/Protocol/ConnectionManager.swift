0de9267f822696cd87c37438ec6714920eb0a435
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-05-14 19:40:43 +0200

    feat(protocol) : add ConnectionManager protocol to test and handle error cases if the user has no connection or has a bad connection

---
+++ Baluchon/Protocol/ConnectionManager.swift
@@ -0,0 +1,34 @@
+//
+//  ConnectionManager.swift
+//  Baluchon
+//
+//  Created by Zidouni on 14/05/2023.
+//
+
+import Foundation
+
+protocol ConnectionManager {
+    func isConnected() -> Bool
+    var isNetworkReachable: Bool { get }
+}
+
+class RealConnectionManager: ConnectionManager {
+    var isNetworkReachable: Bool = true
+
+    func isConnected() -> Bool {
+        return isNetworkReachable
+    }
+}
+
+class NoConnectionManager: ConnectionManager {
+    var isNetworkReachable: Bool = false
+
+    func isConnected() -> Bool {
+        return isNetworkReachable
+    }
+}
+
+
+
+
+
